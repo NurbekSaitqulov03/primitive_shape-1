901f32cfd2567a101f385c491fed0d562d0ece5f
NurbekSaitqulov03 <nurbek.saitqulov23@gmail.com>
2023-02-26 17:02:56 +0500

    circle

--- circle.py
+++ circle.py
@@ -1,3 +1,4 @@
+from math import pi
 class Circle:
     def __init__(self, r) -> None:
         self.r = r
@@ -11,7 +12,8 @@ class Circle:
         Returns:
             float or int: result.
         """
-        pass
+        if self.r > 0:
+            return pi * pow(self.r, 2)
 
     def getLength(self):
         """
@@ -22,4 +24,8 @@ class Circle:
         Returns:
             float or int: result..
         """
-        pass
+        if self.r > 0:
+            return 2 * pi * self.r
+        
+x = Circle(25)
+print(x.getArea())
