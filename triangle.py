0fd24fe5c2e06906eb39c099736dc9870ece26f0
NurbekSaitqulov03 <nurbek.saitqulov23@gmail.com>
2023-02-26 16:33:48 +0500

    triangle.py

--- triangle.py
+++ triangle.py
@@ -1,7 +1,20 @@
 from polygon import Polygon
+from math import sqrt
 
 class Triangle(Polygon):
     def __init__(self, a, b, c):
         self.a = a
         self.b = b
         self.c = c
+    def getArea(self):
+        if self.a > 0 and self.b > 0 and self.c > 0:
+            half_perimeter = (self.a + self.b + self.c)/2
+            return sqrt(half_perimeter * (half_perimeter - self.a) * (half_perimeter - self.b) * (half_perimeter - self.c))
+        return 0
+    def getPerimeter(self):
+        if self.a > 0 and self.b > 0 and self.c > 0:
+            return self.a + self.b + self.c
+        return 0 
+
+x = Triangle(3, 4, 5)
+print(x.getPerimeter())
